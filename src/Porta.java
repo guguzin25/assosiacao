795097430987f713ced956bf624b6db1123392f3
Gustavo Silva <gustavo.dubsantos@gmail.com>
2026-03-16 14:43:59 -0300

    mais um

---
+++ src/Porta.java
@@ -0,0 +1,22 @@
+public class Porta {
+    
+    private String lado;
+
+
+    public Porta(String lado) {
+        this.lado = lado;
+    }
+
+    public String getLado() {
+        return lado;
+    }
+
+    public void setLado(String lado) {
+        this.lado = lado;
+    }
+
+    public String toString() {
+        return "lado=" + lado;
+    }
+
+}
